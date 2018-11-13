7664a5c11c6a60a79fc2e0aac1b3063da786a3d9
靳炳淑 <jinbingshu_nju@qq.com>
2018-11-13 15:11:51 +0800

    Poster 完成

--- src/component/MyPoster/index.vue
+++ src/component/MyPoster/index.vue
@@ -1,17 +1,19 @@
 <template>
-  <el-carousel indicator-position="outside">
-    <el-carousel-item v-for="item in passageList" :key="item.id">
-      <div>
-        <poster-card
-          v-bind:title="item.title"
-          v-bind:coverPicture="item.coverPicture"
-          v-bind:script="item.script"
-          v-bind:type="item.type"
-          v-bind:date="item.date"
-        />
-      </div>
-    </el-carousel-item>
-  </el-carousel>
+  <div class="main">
+    <el-carousel indicator-position="outside" class="body">
+      <el-carousel-item v-for="item in passageList" :key="item.id" class="item">
+        <div>
+          <poster-card
+            v-bind:title="item.title"
+            v-bind:coverPicture="item.coverPicture"
+            v-bind:script="item.script"
+            v-bind:type="item.type"
+            v-bind:date="item.date"
+          />
+        </div>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
 
 <script>
@@ -30,4 +32,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '@/style/default.scss';
+
+.body {
+  width: 100%;
+  height: 100%;
+  border-radius: $default-border-radius;
+  box-shadow: $default-box-shadow;
+
+  .item {
+    height: 100%;
+  }
+}
 </style>
