4437fdf3c825524482ffdf3c98e587ab74f656e3
Starink <161250119@qq.com>
2019-01-21 11:56:56 +0800

    modifyUrl

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://47.101.144.241/3000",
+        target: "http://47.101.144.241:3000/",
         ws: true,
         changeOrigin: true
       }
