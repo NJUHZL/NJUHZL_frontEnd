514d5841e0e17cdfac2262d2bc4ed215704f5a40
Starink <161250119@qq.com>
2019-01-28 11:11:47 +0800

    fix

--- src/layout/defaultLayout.vue
+++ src/layout/defaultLayout.vue
@@ -1,12 +1,10 @@
 <template>
-    <div>
+    <div :style="{minHeight: minHeight+'px'}" style="position: relative">
         <div id="navi">
             <navi></navi>
         </div>
-        <router-view></router-view>
-        <div id="foot">
-            <foot></foot>
-        </div>
+        <router-view id="main"></router-view>
+        <foot id="foot"></foot>
         <div id="rightBar">
             <rightBar></rightBar>
         </div>
@@ -19,7 +17,22 @@ import foot from "@/components/foot.vue";
 import rightBar from "@/components/rightBar.vue";
 export default {
   name: "DefaultLayout",
-  components: { navi, foot, rightBar }
+  data() {
+    return {
+      minHeight: 0
+    };
+  },
+  components: { navi, foot, rightBar },
+  mounted() {
+    this.minHeight = window.screen.availHeight;
+    console.log(this.minHeight);
+    console.log(document.body.scrollHeight);
+    if (document.body.scrollHeight <= this.minHeight) {
+      $("#foot").css("position", "absolute");
+    } else {
+      $("#foot").css("position", "inherit");
+    }
+  }
 };
 </script>
 
--- src/store/modules/passage.module.js
+++ src/store/modules/passage.module.js
@@ -24,8 +24,8 @@ const actions = {
 
   //获得某一类型的文档列表
   async [ACTIONS.FETCH_ONE_CLASS_OF_PASSAGE_LIST](context, className) {
-    const result = await fetchOneClassOfPassageList(className);
     console.log("CLASS_NAME:", className);
+    const result = await fetchOneClassOfPassageList(className);
     console.log(result);
     context.commit(MUTATIONS.SET_ONE_CLASS_OF_PASSAGE_LIST, result);
   },
