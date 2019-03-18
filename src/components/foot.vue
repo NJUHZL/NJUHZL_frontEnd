3872f6e388b0c29dbf11ea994b7a6c95dc43afc1
Starink <161250119@qq.com>
2019-03-18 10:11:26 +0800

    fix

--- src/components/foot.vue
+++ src/components/foot.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="foot">
-        @2018 by <p style="color: white;display: inline-block">Nanjing University</p>   njuhzl@163.com
+        @2018 by <p style="color: white;display: inline-block">Nanjing University</p>
+        <a style="display: inline-block; margin-left: 30px; color: lightgoldenrodyellow" href="http://www.miitbeian.gov.cn/">苏ICP备19010277号-1</a>
     </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,7 @@
     </div>
     <div>
       <carousel style=""></carousel>
-      <banner class="banner" style="margin-top: 120px"/>
-      <div class="page-content">
+      <div class="page-content" style="margin-top: 100px">
         <div style="text-align: center">
           <div class="el-card is-always-shadow function-card">
             <img src="http://njuhzl.oss-cn-hangzhou.aliyuncs.com/static/事实核查.png"/>
@@ -44,7 +43,8 @@
           ></recommendComment>
         </div>
       </div>
-      <div style="text-align: center;margin-top: 50px;padding-bottom: 60px">
+      <banner class="banner" style="margin-top: 120px"/>
+      <div style="text-align: center;padding-bottom: 60px">
         <div class="introBlock">
           <h2><i class="el-icon-success" style="color: lightblue"></i>  新闻核实</h2>
           <p>“核真录”始终免费开放的功能，为公众提供免费的新闻核实、查验服务，对各种假新闻、错误信息、谣言等进行核查和澄清。</p>
