b572bf068df12f1eae82c360e718dc0f09f1c74e
Starink <161250119@qq.com>
2019-07-23 00:38:02 +0800

    try pwa with @cli/pwa

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router/router";
 import store from "./store/index";
+
 // import "./plugin/element.js";
 import axios from "axios";
+
+import './registerServiceWorker'
 // import { getToken } from "./util/token";
 
 Vue.prototype.$axios = axios;
